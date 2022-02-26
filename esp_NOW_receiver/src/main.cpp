91120973feadf6498fcfce80f9e0e20531323c94
Adam <atm88@gmx.de>
2022-02-26 16:49:10 +0100

    minor cleanup

--- esp_NOW_receiver/src/main.cpp
+++ esp_NOW_receiver/src/main.cpp
@@ -3,54 +3,14 @@
 #include <esp_now.h>
 #include <WiFi.h>
 
-//// Structure example to receive data
-//// Must match the sender structure
-//typedef struct struct_message {
-//  int id;
-//  float val;
-////  int x;
-////  int y;
-//}struct_message;
-//
-//// Create a struct_message called myData
-//struct_message myData;
-//
-//// Create a structure to hold the readings from each board
-//struct_message board1;
-//struct_message board2;
-//struct_message board3;
-//
-//// Create an array with all the structures
-//struct_message boardsStruct[3] = {board1, board2, board3};
-
-// callback function that will be executed when data is received
-//void OnDataRecv(const uint8_t * mac_addr, const uint8_t *incomingData, int len) {
 void OnDataRecv(const uint8_t * mac_addr, const uint8_t *incomingData, int len) {
-//  char inStr[len];
-//  char macStr[18];
-//  Serial.print("Packet received from: ");
-//  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
-//           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
-//  Serial.println(macStr);
-//  memcpy(&myData, incomingData, sizeof(myData));
-//  Serial.printf("Board ID %u: %u bytes\n", myData.id, len);
-  // Update the structures with the new incoming data
-//  boardsStruct[myData.id-1].x = myData.x;
-//  boardsStruct[myData.id-1].y = myData.y;
-//  boardsStruct[myData.id-1].val = myData.val;
-//  Serial.printf("x value: %d \n", boardsStruct[myData.id-1].x);
-//  Serial.printf("val: %3f \n", boardsStruct[myData.id-1].val);
-//  Serial.printf("y value: %d \n", boardsStruct[myData.id-1].y);
 
   Serial.println(char(incomingData[0]));
 
   for(int i = 2; i < len; i++)
   {
     Serial.print(char(incomingData[i]));
-//    inStr[i] = char(incomingData[i]);
   }
-
-//  Serial.println();
 }
  
 void setup() {
@@ -72,13 +32,5 @@ void setup() {
 }
  
 void loop() {
-  // Acess the variables for each board
-  /*int board1X = boardsStruct[0].x;
-  int board1Y = boardsStruct[0].y;
-  int board2X = boardsStruct[1].x;
-  int board2Y = boardsStruct[1].y;
-  int board3X = boardsStruct[2].x;
-  int board3Y = boardsStruct[2].y;*/
-
   delay(10000);  
 }
