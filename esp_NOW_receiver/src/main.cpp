f78c89066e75e543d2037d09fd971c3c73e40e45
Adam <atm88@gmx.de>
2022-03-06 14:18:34 +0100

    matlab as single chars (wrong)

--- esp_NOW_receiver/src/main.cpp
+++ esp_NOW_receiver/src/main.cpp
@@ -34,6 +34,8 @@
 #include <WiFi.h>
 
 float arr[64] = {0};
+char matlab_arr[453] = {0};    // 64*7 + 7
+char empty_matlab_arr[453] = {0};    // 64*7 + 7
 
 void OnDataRecv(const uint8_t * mac_addr, const uint8_t *incomingData, int len) {
 
@@ -41,13 +43,19 @@ void OnDataRecv(const uint8_t * mac_addr, const uint8_t *incomingData, int len) 
 //  float arr[64] = {0};
 //  for(int i=0; i<64; i++) Serial.print(arr[i]);
 
+  matlab_arr[5] = ',';
   if(char(incomingData[0]) == 'C')
   {
-    Serial.println(char(incomingData[0]));
+    char temp_arr[len-2] = {};
+    char empty_temp_arr[len-2] = {};
     for(int i = 2; i < len; i++)
     {
-      Serial.print(char(incomingData[i]));
+      temp_arr[i-2] = char(incomingData[i]);
+      matlab_arr[i-2] = char(incomingData[i]);
     }
+//    matlab_arr[0] = atoi(temp_arr);
+//    for(int j=0; j<len-2; j++) Serial.print(temp_arr[j]);
+    memcpy(temp_arr, empty_temp_arr, len-2);
   }
   else
   {
@@ -61,46 +69,78 @@ void OnDataRecv(const uint8_t * mac_addr, const uint8_t *incomingData, int len) 
 
     for(int i = 0; i < len; i++)
     {
-      Serial.print(char(incomingData[i]));
+//      Serial.print(char(incomingData[i]));
+//      Serial.println();
       if(char(incomingData[i]) == ':')
       {
-//        Serial.println(i);
-//        Serial.println(temp_arr_1);
         temp_val_1 = atoi(temp_arr_1);
-//        Serial.println(temp_val_1);
 
         temp_cnt = i;
-//        temp_cnt = strlen(temp_arr_1);
 
         memcpy(temp_arr_1, empty_temp_arr_1, 3);
       }
       if(i > temp_cnt)
       {
+//        matlab_arr[strlen(matlab_arr+1)] = char(incomingData[i]);
+//        matlab_arr[i-temp_cnt-1+5] = char(incomingData[i]);
         temp_arr_2[i-temp_cnt-1] = char(incomingData[i]);
         if(i == len-1)
         {
-          Serial.println("LEL");
           temp_val_2 = atoi(temp_arr_2);
-          Serial.println(temp_val_1);
-          Serial.println(temp_val_2);
-          Serial.println(temp_arr_2);
           arr[temp_val_1] = temp_val_2;
 
-//          Serial.println(arr);
-          for(int j=0; j<64; j++) Serial.print(arr[j]);
+//          for(int j=0; j<64; j++) Serial.print(arr[j]);               // irgendwo hier werden die Nachkommastellen zu 0 ????????????????
+//          Serial.println();
+          char test_temp[10] = {};
+          char empty_test_temp[10] = {};
+          for(int k=0; k<64; k++)
+          {
+            if(arr[k] <= 0.001)
+            {
+              sprintf(test_temp, "%.2f", 1.00);
+            }
+            else
+            {
+              sprintf(test_temp, "%.2f", arr[k]);
+            }
+//            sprintf(test_temp, "%.2f", arr[k]);
+//            Serial.println(test_temp);
+            for(int g=0; g<strlen(test_temp); g++)
+            {
+              matlab_arr[(k*(strlen(test_temp)+1))+6+g] = test_temp[g];
+//              matlab_arr[(k*strlen(test_temp))+7+g] = test_temp[g];
+              //matlab_arr[k+6+g] = char(k+6+g);
+//              Serial.print((k*strlen(test_temp))+6+g);
+//              Serial.println(test_temp);
+            }
+//            matlab_arr[(k*strlen(test_temp))+7+strlen(test_temp)] = ',';
+            matlab_arr[(k*(strlen(test_temp)+1))+6+strlen(test_temp)] = ',';
+            memcpy(test_temp, empty_test_temp, 10);
+          }
 
           memcpy(temp_arr_2, empty_temp_arr_2, 5);
           temp_val_1 = 0;
           temp_val_2 = 0;
           temp_cnt = 3;
         }
-      } else {
+//        for(int k=1; k<65; k++) matlab_arr[k] = arr[k-1];
+      }
+      else
+      {
         temp_arr_1[i] = char(incomingData[i]);
       }
     
     }
 
   }
+
+//  Serial.println("MATLAB ARR:");
+//  Serial.println(matlab_arr);
+//  Serial.println("-----------");
+//  memcpy(matlab_arr, empty_matlab_arr, strlen(matlab_arr));
+
+  for(int h=0; h<strlen(matlab_arr); h++) Serial.print(matlab_arr[h]);
+  Serial.println();
 
 }
  
